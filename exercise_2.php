c5d1401e8792e0766e17b13b8c56bae85974080c
Konstantin <byshkin.konstantin@mail.ru>
2018-11-26 23:03:31 +0300

    Выполнил конкатинацию двух строк

---
+++ exercise_2.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ENERGIEKOST
+ * Date: 26.11.2018
+ * Time: 22:50
+ */
+echo "'Tomorrow I \'ll learn PHP global variables.' \n". "'This is a bad command : del c:\\*.*' ";
+
+// \n - new line новая строка
+// \t - tab отступ
+
+?>
+
