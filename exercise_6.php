7afa0d8a21f9054140ef93b828b716ba85e99211
Konstantin <byshkin.konstantin@mail.ru>
2018-11-27 23:56:59 +0300

    //вывел user  agent, узнал какой браузер и версия у пользователя.

---
+++ exercise_6.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ENERGIEKOST
+ * Date: 27.11.2018
+ * Time: 23:39
+ */
+
+//вывел user  agent, узнал какой браузер и версия у пользователя.
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+echo $user_agent;
