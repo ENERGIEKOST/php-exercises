00c108e954bba6445ba45566184d4534d51a222f
Konstantin <byshkin.konstantin@mail.ru>
2018-11-29 14:27:30 +0300

    Сделал 3 заданиями с функицкей

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+function multiply($shit, $fuck) {
+    return $shit * $fuck;
+}
+
+echo multiply (4, 4).'</br>';
+
+function custom($one, $two) {
+     return $one + $two + 5;
+}
+
+echo custom(3,6).'</br>';
+
+function custom_3($man, $women) {
+    $man_women = $man - $women;
+    return multiply ($man_women, 2);
+}
+
+echo custom_3(6,2);
